df38397f65d231e532e0018715618641784f1222
Michel Momose <michelmomose@gmail.com>
2023-10-03 21:24:31 -0300

    Criado a relação de classes no arquivo java, para ter como base do desenvolvimento

---
+++ Aplicação Java/GerenciamentoTG/src/Turma.java
@@ -0,0 +1,17 @@
+public class Turma {
+    
+    private String semestre_ano;
+
+    public Turma(String semestre_ano) {
+        this.semestre_ano = semestre_ano;
+    }
+
+    public String getSemestre_ano() {
+        return semestre_ano;
+    }
+
+    public void setSemestre_ano(String semestre_ano) {
+        this.semestre_ano = semestre_ano;
+    }
+
+}
