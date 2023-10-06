ac8d3ea2c6a43f76f2a3288cbf87274a96267fd0
Renan Graciano Faria <111203231+VonNexx@users.noreply.github.com>
2023-10-06 20:05:06 -0300

    Create Agendamentos.fxml

--- Aplicação Java/GerenciamentoTG/src/App.java
+++ Aplicação Java/GerenciamentoTG/src/App.java
@@ -1,14 +1,22 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application{
     public static void main(String[] args) {
-        
+        launch(args);
     }
 
     @Override
-    public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Home.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setTitle("Infinity Dev");
+        primaryStage.setScene(tela);
+        primaryStage.show();
     }
 }
