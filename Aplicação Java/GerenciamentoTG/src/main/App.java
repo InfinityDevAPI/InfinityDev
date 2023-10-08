b878aed047177841852baa309fa0056748c9a88b
Michel Momose <michelmomose@gmail.com>
2023-10-08 12:36:13 -0300

    adicionado classes

---
+++ Aplicação Java/GerenciamentoTG/src/main/App.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Home.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setTitle("Infinity Dev");
+        primaryStage.setScene(tela);
+        primaryStage.show();
+    }
+}
---
+++ Aplicação Java/GerenciamentoTG/src/main/Orientador.java
@@ -0,0 +1,16 @@
+public class Orientador {
+    
+    private String nome;
+
+    public Orientador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
