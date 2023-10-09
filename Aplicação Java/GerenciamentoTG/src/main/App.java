9b9e13d0821d517bb369f2108035ba7725340231
Renan Graciano Faria <111203231+VonNexx@users.noreply.github.com>
2023-10-09 12:12:03 -0300

    Bug Fix

--- Aplicação Java/GerenciamentoTG/src/main/App.java
+++ Aplicação Java/GerenciamentoTG/src/main/App.java
@@ -7,18 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application{
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Home.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene tela = new Scene(root);
-
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         primaryStage.setTitle("Infinity Dev");
-        primaryStage.setScene(tela);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
