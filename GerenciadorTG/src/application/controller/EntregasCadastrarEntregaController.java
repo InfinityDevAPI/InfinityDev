dda78ea0ebefa28ff02921dab3935209467e776b
ropcastr <ropcastr@gmail.com>
2023-10-20 22:50:51 -0300

    Criado Estrutura de Projecto JavaFX compativel com Eclipse e vscode

---
+++ GerenciadorTG/src/application/controller/EntregasCadastrarEntregaController.java
@@ -0,0 +1,116 @@
+package application.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class EntregasCadastrarEntregaController {
+
+    @FXML
+    private TextArea Descrição_Entrega;
+
+    @FXML
+    private Button botaoCadastrarEntrega;
+
+    @FXML
+    private Button botaoCadastrarNota;
+
+    @FXML
+    private Button botaoConfirmar;
+
+    @FXML
+    private Button botaoDevolutiva;
+
+    @FXML
+    private Button botaoHome;
+
+    @FXML
+    private Button dropdownAluno;
+
+    @FXML
+    private Button dropdownAtividade;
+
+    @FXML
+    private Button dropdownCalendario;
+
+    @FXML
+    void abrirCadastrarNota(MouseEvent event) {
+        try {
+
+            Stage currentStage = (Stage) botaoCadastrarNota.getScene().getWindow();
+            
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/ui/EntregasCadastrarNota.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Cadastro de Notas");
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            
+            stage.initModality(Modality.APPLICATION_MODAL);
+            
+            currentStage.close();
+            
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+    }
+
+    @FXML
+    void abrirDevolutiva(MouseEvent event) {
+        try {
+
+            Stage currentStage = (Stage) botaoDevolutiva.getScene().getWindow();
+            
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/ui/EntregasDevolutiva.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Cadastro de Devolutiva");
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            
+            stage.initModality(Modality.APPLICATION_MODAL);
+            
+            currentStage.close();
+            
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+    }
+
+    @FXML
+    void abrirHome(MouseEvent event) {
+        try {
+
+            Stage currentStage = (Stage) botaoHome.getScene().getWindow();
+            
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/ui/Home.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Gerenciador de Tg's");
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            
+            stage.initModality(Modality.APPLICATION_MODAL);
+            
+            currentStage.close();
+            
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+    }
+    
+}
