39b7fa1bd51fe61f0b2547e87e03c3383f9adddb
Julio Cesar Barbosa da Rosa Rodrigues <juliojcbarbosa2003@gmail.com>
2023-10-31 21:42:00 -0300

    Finalizado a classe para correção de dados

--- GerenciadorTG/src/application/controller/CorrecaoCSV.java
+++ GerenciadorTG/src/application/controller/CorrecaoCSV.java
@@ -13,8 +13,8 @@ import java.util.Date;
 public class CorrecaoCSV {
     public static void main(String[] args) {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("Arquivo CSV Formatado.csv"));
-            BufferedWriter bw = new BufferedWriter(new FileWriter("arquivocorrigido.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("dados.csv"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("dados_corrijidos.csv"));
 
             String linha;
             boolean isHeader = true;
@@ -28,21 +28,22 @@ public class CorrecaoCSV {
 
                 String[] campos = linha.split(",");
 
-                if (campos.length >= 6) {
+                if (campos.length >= 11) {
                     Timestamp timestamp = corrigirTimestamp(campos[0]);
-                    String emailAddress = campos[1]; 
-                    String emailFatec = campos[2];
-                    String nomeCompleto = campos[3];
-                    String nomeCompletoOrientador = campos[4];
-                    String emailFatecOrientador = campos[5];
+                    String emailAddress = corrigirEmail(campos[1]);
+                    String emailFatec = corrigirEmail(campos[2]);
+                    String nomeCompleto = corrigirNome(campos[3]);
+                    String nomeCompletoOrientador = corrigirNome(campos[4]);
+                    String emailFatecOrientador = corrigirEmail(campos[5]);
+                    String matriculadoEm = campos[6];
+                    String tipoDeTG = campos[7];
+                    String problemaASerResolvido = campos[8];
+                    String empresaEnvolvida = campos[9];
+                    String disciplina = corrigirNome(campos[10]);
 
-                    // Substitua "null" por uma data válida ou uma string vazia se não houver data
-                    if (timestamp == null) {
-                        timestamp = new Timestamp(System.currentTimeMillis());
-                    }
-
-                    bw.write(timestamp + "," + emailAddress + "," + emailFatec + "," + nomeCompleto + "," + nomeCompletoOrientador + "," +
-                            emailFatecOrientador + ",");
+                    bw.write(timestamp + "," + emailAddress + "," + emailFatec + "," + nomeCompleto + ","
+                            + nomeCompletoOrientador + "," + emailFatecOrientador + "," + matriculadoEm + ","
+                            + tipoDeTG + "," + problemaASerResolvido + "," + empresaEnvolvida + "," + disciplina);
                     bw.newLine();
                 }
             }
@@ -56,16 +57,15 @@ public class CorrecaoCSV {
 
     public static Timestamp corrigirTimestamp(String string) {
         try {
+            // Remova as aspas duplas se estiverem presentes
+            string = string.replace("\"", "");
+    
             if (string != null && !string.trim().isEmpty() && !string.equalsIgnoreCase("null")) {
-                SimpleDateFormat inputFormat = new SimpleDateFormat("M/dd/yyyy H:mm:ss");
-                SimpleDateFormat outputFormat = new SimpleDateFormat("M/dd/yyyy H:mm:ss");
-
+                SimpleDateFormat inputFormat = new SimpleDateFormat("M/d/yyyy H:mm:ss");
                 Date parsedDate = inputFormat.parse(string);
-                String formattedDate = outputFormat.format(parsedDate);
-
-                return Timestamp.valueOf(formattedDate);
+    
+                return new Timestamp(parsedDate.getTime());
             } else {
-                // Se a data estiver vazia ou "null", retorne null
                 return null;
             }
         } catch (ParseException e) {
@@ -73,15 +73,13 @@ public class CorrecaoCSV {
             return null;
         }
     }
-    // Função para corrigir o nome (primeira letra maiúscula, sem números)
+    
     public static String corrigirNome(String nome) {
         nome = nome.trim();
         nome = nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase();
-        nome = nome.replaceAll("[0-9]", "");
         return nome;
     }
 
-    // Função para corrigir o email (letras minúsculas)
     public static String corrigirEmail(String email) {
         return email.toLowerCase();
     }
